12d1425690eafc9a8f29136897902c5a1f17b3af
Swithin Okyere <jayswithin@gmail.com>
2022-07-17 00:17:54 +0000

    [UPDATE] - Formatted posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Posts({ posts, loading }) {
   if (loading) {
-    return <h2>Posts Loading....</h2>
+    return <h2>Posts Loading....</h2>;
   }
-  
+
   return (
     <div>
-      {
-        posts.map((post) => (
-          <ul className="border">
-            <li key={post.id} className="p-2">
-              {post.title}
-            </li>
-
-          </ul>
-        ))
-      }
+      {posts.map((post) => (
+        <ul className="border">
+          <li key={post.id} className="p-2">
+            {post.title}
+          </li>
+        </ul>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
